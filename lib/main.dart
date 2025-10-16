a0286459e28b256f696ccfced2b87c61726d4816
Nao Joy <naojoy5@gmail.com>
2025-10-16 15:33:21 -0700

    navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static const appTitle = 'Burger bar'
 
   // This widget is the root of your application.
   @override
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         '/homepage': (context) => const HomePage(),
         '/articlespage': (context) => const ArticlesPage(),
       },
-      title: 'Flutter Demo',
+      title: appTitle,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -39,7 +40,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: appTitle),
     );
   }
 }
@@ -62,19 +63,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> { 
 
   @override
   Widget build(BuildContext context) {
@@ -93,8 +82,57 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Burgers for everyone'),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              }
+            )
+          }
+        )
       ),
+      drawer: Drawer(
+        backgroundColor: Colors.deepPurple[100]
+        child: ListView( // lets user scroll through options if they need more vertical space
+          // remove padding from ListView
+          padding: EdgeInsets.zero
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blueGrey),
+              child: Text('Brioche buns'),
+            ),
+            ListTile(
+              title: const Text('Lettuce'),
+              onTap: () {
+                // update state of the app
+                // then close the drawer
+                Navigator.pop(context)
+              },
+            ),
+            ListTile(
+              title: const Text('Tomato'),
+              onTap: () {
+                // update state of the app
+              }
+            )
+            ListTile(
+              title: const Text('Patty'),
+              onTap: () {
+                // update state of the app
+              }
+            )
+            ListTile(
+              title: const Text('Cheese'),
+              onTap: () {
+                // update state of the app
+              }
+            )
+          ]
+        )
+      )
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -113,29 +151,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-
-            ElevatedButton(onPressed: () {
-              // if (kDebugMode) {
-              //   debugPrint("button pressed $_counter times");                  
-              // }
-              print("button pressed $_counter times");
-            }, 
-            child: Text('next')),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),
-                ),
-                );
-              }, child: child)
-          ],
+          
         ),
       ),
       floatingActionButton: FloatingActionButton(
