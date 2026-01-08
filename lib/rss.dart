efe2bbdc105f3e1be3a877725a249dd227d901b5
Eliot <elogan3600@student.pps.net>
2026-01-08 14:57:29 -0800

    webfeed is dead.

--- lib/rss.dart
+++ lib/rss.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: avoid_print
 
-import 'package:webfeed/webfeed.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
