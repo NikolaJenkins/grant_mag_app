ef3f78cf36935af91115723701be55a47acbc590
Nao Joy <naojoy5@gmail.com>
2026-02-09 15:20:54 -0800

    This is ELIOT: I'M THE BEST! I SOLVED NAMESPACES ISUSEBUBEYS! $$$$$$$$$$$$$$$ (Mandell, this is actually Eliot. I was working on Nao's computer today)

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -15,12 +15,24 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (
+                requested.id.id == "com.android.application" ||
+                requested.id.id == "com.android.library"
+            ) {
+                useModule("com.android.tools.build:gradle:7.4.2")
+            }
+            if (requested.id.id == "org.jetbrains.kotlin.android") {
+                useVersion("1.8.22")
+            }
+        }
+    }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
