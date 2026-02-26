58c34527428d4b4260bf45364f9571b8dcc8d839
everythingcover <noahgrahambrown@gmail.com>
2026-02-26 14:59:51 -0800

    minor tweaks before upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:grant_mag_app/settings.dart';
 import 'package:grant_mag_app/profile.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-////import 'package:flutter_checklist/checklist.dart';
+//import 'package:flutter_checklist/checklist.dart';
 import 'package:grant_mag_app/noti_service.dart';
 import 'rss.dart';
 
